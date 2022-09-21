7ee85d68417001a884ac54df670844151670e1a3
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2022-09-21 07:13:02 +0000

    Revert "emugl_config: fix potential uses of nullptr"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (go/aca-get/a80cf050-8869-4ddd-a06d-ea9a3524e298). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I671fc64dc1bcac3c22e1322945128569fb64dd88

--- host-common/opengl/emugl_config.cpp
+++ host-common/opengl/emugl_config.cpp
@@ -262,11 +262,11 @@ bool emuglConfig_init(EmuglConfig* config,
         return true;
     }
 
-    if (gpu_mode && !strcmp("angle", gpu_mode)) {
+    if (!strcmp("angle", gpu_mode)) {
         gpu_mode = "angle_indirect";
     }
 
-    if (gpu_mode && !strcmp("swiftshader", gpu_mode)) {
+    if (!strcmp("swiftshader", gpu_mode)) {
         gpu_mode = "swiftshader_indirect";
     }
 
@@ -282,7 +282,7 @@ bool emuglConfig_init(EmuglConfig* config,
     // the best mode depending on the environment. Its purpose is to
     // enable 'swiftshader' mode automatically when NX or Chrome Remote Desktop
     // is detected.
-    if ((gpu_mode && !strcmp(gpu_mode, "auto")) || host_set_in_hwconfig) {
+    if (!strcmp(gpu_mode, "auto") || host_set_in_hwconfig) {
         // The default will be 'host' unless:
         // 1. NX or Chrome Remote Desktop is detected, or |no_window| is true.
         // 2. The user's host GPU is on the blacklist.
@@ -343,10 +343,9 @@ bool emuglConfig_init(EmuglConfig* config,
     // 'host' is a special value corresponding to the default translation
     // to desktop GL, 'guest' does not use host-side emulation,
     // anything else must be checked against existing host-side backends.
-    if (!gpu_mode ||
-        (strcmp(gpu_mode, "host") != 0 && strcmp(gpu_mode, "guest") != 0)) {
+    if (strcmp(gpu_mode, "host") != 0 && strcmp(gpu_mode, "guest") != 0) {
         const std::vector<std::string>& backends = sBackendList->names();
-        if (!gpu_mode || !stringVectorContains(backends, gpu_mode)) {
+        if (!stringVectorContains(backends, gpu_mode)) {
             std::string error = StringFormat(
                 "Invalid GPU mode '%s', use one of: host swiftshader_indirect. "
                 "If you're already using one of those modes, "
