899b2c59810c8894152444e87ce083a91ca52410
Joshua Duong <joshuaduong@google.com>
2022-10-12 08:20:09 -0700

    Fix qemu build when using aemu/base.
    Bug: 237805597
    Test: presubmit
    Change-Id: I211955d8478d0707e1653a84fc702a76cf25575b

--- base/include/aemu/base/files/PathUtils.h
+++ base/include/aemu/base/files/PathUtils.h
@@ -185,6 +185,9 @@ public:
 
     // Return the root prefix for the current platform. See above for
     // documentation.
+    static size_t rootPrefixSize(const char* path, HostType hostType) {
+        return rootPrefixSize(path ? std::string(path) : std::string(""), hostType);
+    }
     static size_t rootPrefixSize(const char* path) {
         return rootPrefixSize(path, HOST_TYPE);
     }
@@ -268,10 +271,6 @@ public:
     static std::vector<std::string> decompose(const std::string& path,
                                               HostType hostType);
 
-    template <class String>
-    static std::vector<String> decompose(const String& path,
-                                         HostType hostType);
-
     // Decompose |path| into individual components for the host platform.
     // See comments above for more details.
     static std::vector<std::string> decompose(std::string&& path) {
@@ -326,10 +325,6 @@ public:
                                   const std::string& path) {
         return relativeTo(base, path, HOST_TYPE);
     }
-    static std::string relativeTo(const char* base, const char* path, HostType hostType);
-    static std::string relativeTo(const char* base, const char* path) {
-        return relativeTo(base, path, HOST_TYPE);
-    }
 
     static Optional<std::string> pathWithoutDirs(const char* name);
     static Optional<std::string> pathToDir(const char* name);
@@ -343,6 +338,9 @@ public:
     // xx if it exists. Returns kNullopt if the environment variable is
     // not set or empty.
     static Optional<std::string> pathWithEnvSubstituted(std::vector<std::string> decomposedPath);
+
+    // Move a file. It works even when from and to are on different disks.
+    static bool move(const std::string& from, const std::string& to);
 
 #ifdef _WIN32
     static Win32UnicodeString asUnicodePath(const char* path) { return Win32UnicodeString(path); }
