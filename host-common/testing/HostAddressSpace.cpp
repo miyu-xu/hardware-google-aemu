9b46aa71fa161732eedd7ae90d97503d15cd155e
Roman Kiryanov <rkir@google.com>
2024-01-23 17:37:33 -0800

    Support variable guest page size in goldfish_address_space
    This is required for 16K kernels
    
    Bug: 322026591
    Test: presubmit
    Change-Id: I52801be79ac0dde3eb03a28980d858f75095328b
    Signed-off-by: Roman Kiryanov <rkir@google.com>

--- host-common/testing/HostAddressSpace.cpp
+++ host-common/testing/HostAddressSpace.cpp
@@ -479,6 +479,10 @@ uint64_t HostAddressSpaceDevice::getPhysAddrStart() {
     return HostAddressSpaceDevice::getImpl()->getPhysAddrStart();
 }
 
+uint32_t HostAddressSpaceDevice::getGuestPageSize() {
+    return 4096;
+}
+
 int HostAddressSpaceDevice::allocSharedHostRegionFixedLocked(uint64_t page_aligned_size, uint64_t offset) {
     return HostAddressSpaceDevice::getImpl()->allocSharedHostRegionFixedLocked(page_aligned_size, offset);
 }
@@ -490,6 +494,7 @@ static const AddressSpaceHwFuncs sAddressSpaceHwFuncs = {
     &HostAddressSpaceDevice::freeSharedHostRegionLocked,
     &HostAddressSpaceDevice::getPhysAddrStart,
     &HostAddressSpaceDevice::getPhysAddrStart,
+    &HostAddressSpaceDevice::getGuestPageSize,
     &HostAddressSpaceDevice::allocSharedHostRegionFixedLocked,
 };
 
