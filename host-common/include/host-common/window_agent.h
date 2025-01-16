d5655df63bbd6b7c558c59982f7d06362cf1af6c
Rezaul Barbhuiya <rezaulb@google.com>
2025-01-15 17:57:38 -0800

    Add Xr head events, and Xr options to window agent interface.
    DO NOT REVIEW
    NOT READY
    
    Change-Id: Iedac03098b2aaedb64eae5f24902872c60dcee8c

--- host-common/include/host-common/window_agent.h
+++ host-common/include/host-common/window_agent.h
@@ -138,10 +138,25 @@ typedef struct QAndroidEmulatorWindowAgent {
     bool (*userSettingIsDontSaveSnapshot)(void);
     void (*setUserSettingIsDontSaveSnapshot)(bool);
 
+    // Sets the XR input mode.
     bool (*setXrInputMode)(int);
+    // Sets the XR environment mode.
     bool (*setXrEnvironmentMode)(int);
+    // Sets the XR screen recenter.
     bool (*setXrScreenRecenter)();
+    // Sets the XR viewport control mode.
     bool (*setXrViewportControlMode)(int);
+
+    // Sets the XR head rotation event.
+    bool (*setXrHeadRotationEvent)(float x,float y, float z);
+    // Sets the XR head movement event.
+    bool (*setXrHeadMovementEvent)(float delta_x, float delta_y, float delta_z);
+    // Sets the XR head angular velocity event.
+    bool (*setXrHeadAngularVelocityEvent)(float omega_x, float omega_y, float omega_z, float transition_time_sec);
+    // Sets the XR head velocity event.
+    bool (*setXrHeadVelocityEvent)(float x, float y, float z, float transition_time_sec);
+    // Sets the XR options.
+    bool (*setXrOptions)(int environment, float passthroughCoefficient);
 
 } QAndroidEmulatorWindowAgent;
 
