913ee6d67b66c65b004fee98190b2f99e1085514
Bo Hu <bohu@google.com>
2025-02-12 18:41:23 -0800

    add vulkanInstanceEnumerate to vm interface
    So to get a list of vulkan instance ids and
    their app names.
    
    Change-Id: Ibf5df6e2596b1fb9bdd5f2d52558573599fd955c

--- host-common/include/host-common/vm_operations.h
+++ host-common/include/host-common/vm_operations.h
@@ -272,6 +272,7 @@ typedef struct QAndroidVmOperations {
 
     void (*vulkanInstanceRegister)(uint64_t id, const char* name);
     void (*vulkanInstanceUnregister)(uint64_t id);
+    void (*vulkanInstanceEnumerate)(uint32_t* pCount, uint64_t* pIds, const char** pNames);
 
     // Set the reason to skip snapshotting on exit.
     void (*setSkipSnapshotSaveReason)(SnapshotSkipReason reason);
