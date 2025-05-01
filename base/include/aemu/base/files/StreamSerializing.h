a94aa914e43447360c1692d31fd2ac5554688825
Jason Macnak <natsu@google.com>
2025-05-01 17:20:53 +0000

    Update [load|save]Buffer to handle aemu and gfxstream SmallVector
    ... while Gfxstream interface transitions completely to Gfxstream
    based files to be standalone.
    
    Bug: b/403363880
    
    Test: cd hardware/google/gfxstream
          mma
    
    Test: cd hardware/google/gfxstream
          mkdir build && cd build
          cmake .. -G Ninja
          ninja
    
    Test: meson setup \
            -Ddefault_library=static \
            -Dgfxstream-build=host \
            build
    
          meson compile -C build
    
    Test: cd <emu master dev repo>
          cd external/qemu
          python android/build/python/cmake.py --gfxstream
    
    Test: cd <emu main next repo>
          prebuilts/bazel/linux-x86_64/bazel test \
          --platforms=@//build/bazel/platforms:linux_x64 \
         @gfxstream//...
    Change-Id: I13bfbc4d52e453b7608f291c2001ddb56f4f31f2

--- base/include/aemu/base/files/StreamSerializing.h
+++ base/include/aemu/base/files/StreamSerializing.h
@@ -49,19 +49,21 @@ bool loadBuffer(Stream* stream, std::vector<T>* buffer) {
     return ret == len * sizeof(T);
 }
 
-template <class T, class = enable_if<std::is_standard_layout<T>>>
-void saveBuffer(Stream* stream, const SmallVector<T>& buffer) {
+template <class Container,
+          class = enable_if<std::is_standard_layout<typename Container::value_type>>>
+void saveBuffer(Stream* stream, const Container& buffer) {
     stream->putBe32(buffer.size());
-    stream->write(buffer.data(), sizeof(T) * buffer.size());
+    stream->write(buffer.data(), sizeof(typename Container::value_type) * buffer.size());
 }
 
-template <class T, class = enable_if<std::is_standard_layout<T>>>
-bool loadBuffer(Stream* stream, SmallVector<T>* buffer) {
+template <class Container,
+          class = enable_if<std::is_standard_layout<typename Container::value_type>>>
+bool loadBuffer(Stream* stream, Container* buffer) {
     auto len = stream->getBe32();
     buffer->clear();
     buffer->resize_noinit(len);
-    int ret = (int)stream->read(buffer->data(), len * sizeof(T));
-    return ret == len * sizeof(T);
+    int ret = (int)stream->read(buffer->data(), len * sizeof(typename Container::value_type));
+    return ret == len * sizeof(typename Container::value_type);
 }
 
 template <class T, class SaveFunc>
