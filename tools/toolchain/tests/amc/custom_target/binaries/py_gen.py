d2e8a47bdcbab841484d91c44f620fa6281c5a93
Joshua Duong <joshuaduong@google.com>
2025-11-04 19:11:39 +0000

    amc_test: Add custom_target_test.
    Add a test with a meson build using custom_target.
    
    Bug: 428923963
    Test: bazel test //hardware/google/aemu/tools/toolchain/tests/amc:custom_target_test
    Change-Id: I002dd60cc3aa28cb4cb2b8e4e32433f7880f1d58

---
+++ tools/toolchain/tests/amc/custom_target/binaries/py_gen.py
@@ -0,0 +1,34 @@
+# Copyright 2025 - The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the',  help='License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an',  help='AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import argparse
+from pathlib import Path
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate a C++ header file with a message.')
+    parser.add_argument('-m', '--message', required=True, help='The message to include in the header file.')
+    parser.add_argument('-o', '--output', required=True, help='The output header file path.')
+    args = parser.parse_args()
+
+    out = Path(args.output).absolute()
+
+    print(f"Writing {out}")
+    with open(out, 'w') as f:
+        f.write(f'#include <string>\n\n')
+        f.write(f'namespace generated {{\n')
+        f.write(f'  std::string msg = "{args.message}";\n')
+        f.write(f'}}\n')
+
+if __name__ == '__main__':
+    main()
