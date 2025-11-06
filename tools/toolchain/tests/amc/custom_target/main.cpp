3b780f74a78867999187ed207ef2a1cd748277aa
Joshua Duong <joshuaduong@google.com>
2025-11-06 16:12:09 +0000

    custom_target_test: Add test for binary resolved from pkg-config.
    wayland-scanner.pc does this, mesa tries to use it, and our
    generator does not like it.
    
    Bug: 428923963
    Test: bazel test //hardware/google/aemu/tools/toolchain/tests/amc:custom_target_test
    Change-Id: I2dafbe0f2b59723bb7ba899046a211b26ba24252

--- tools/toolchain/tests/amc/custom_target/main.cpp
+++ tools/toolchain/tests/amc/custom_target/main.cpp
@@ -1,10 +1,12 @@
 #include "message.h"
 #include "msg_1.h"
+#include "my_pkgconfig_header.h"
 
 #include <iostream>
 
 int main(int argc, char *argv[]) {
   std::cout << generated::msg0 << std::endl;
   std::cout << generated::msg1 << std::endl;
+  std::cout << MY_PKGCONFIG_GREETING << std::endl;
   return 0;
 }
